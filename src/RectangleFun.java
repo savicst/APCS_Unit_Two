e2ca95e865bebd6b10a0519f603b5310c0d6f4fb
savicst <savicst@ssfs.org>
2021-09-29 10:42:41 -0400

    Completed RectangleFun.

--- src/RectangleFun.java
+++ src/RectangleFun.java
@@ -1,3 +1,4 @@
+import javax.xml.stream.Location;
 import java.awt.*;
 import java.util.Scanner;
 
@@ -8,14 +9,26 @@ public class RectangleFun {
         Scanner scan = new Scanner(System.in);
 
         // Get input for x, y, width, and height. Create four separate variables for these.
-
+        System.out.print("X: ");
+        int xVal = scan.nextInt();
+        System.out.print("Y: ");
+        int yVal = scan.nextInt();
+        System.out.print("Width: ");
+        int widthVal = scan.nextInt();
+        System.out.print("Height: ");
+        int heightVal = scan.nextInt();
 
         // Create a Rectangle object using the four pieces of user input.
-
+        Rectangle rex = new Rectangle(xVal, yVal, widthVal, heightVal);
 
         // Use the appropriate methods (getWidth() and getHeight() ) to calculate the perimeter, then print it
+        double perimeter = 2 * rex.getWidth() + 2 * rex.getHeight();
+        System.out.println("Perimeter = " + perimeter);
 
         // Use getX(), getY(), and setLocation() to move the Rectangle. Print the new location.
-
+        double newx = -4 + rex.getX();
+        double newy = -2 + rex.getY();
+        rex.setLocation((int) newx, (int) newy);
+        System.out.println("New location is (" + rex.getX() + ", " + rex.getY() + ")" );
     }
 }
