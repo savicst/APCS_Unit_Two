26761810792cb9ebfe33a59da59f0971a6278e2e
savicst <savicst@ssfs.org>
2021-10-13 15:16:33 -0400

    kinda started email generator

--- src/EmailGenerator.java
+++ src/EmailGenerator.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
 public class EmailGenerator {
+    public static void main(String[] args){
+
+    public static String makeUserName(String name){
+        String initial = fullName.substring(0,0);
+        int space = fullName.indexOf(" ");
+        String last = fullName.substring(space);
+        int num = Math.random()
+        }
+    public static String makeEmail(String user, String provider){
+
+        }
+       Scanner scan= new Scanner(System.in);
+        System.out.println("Enter your first and last name: ");
+        String fullName = scan.nextLine();
+
+
+    }
 }
