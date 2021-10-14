5d8c1f40bc8c345ebde6cdbf2d5e2f7a51ad0a5f
savicst <savicst@ssfs.org>
2021-10-14 14:03:21 -0400

    Completed EmailGenerator

--- src/EmailGenerator.java
+++ src/EmailGenerator.java
@@ -1,20 +1,30 @@
 import java.util.Scanner;
 public class EmailGenerator {
-    public static void main(String[] args){
-
-    public static String makeUserName(String name){
-        String initial = fullName.substring(0,0);
-        int space = fullName.indexOf(" ");
-        String last = fullName.substring(space);
-        int num = Math.random()
-        }
-    public static String makeEmail(String user, String provider){
-
-        }
-       Scanner scan= new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
         System.out.println("Enter your first and last name: ");
         String fullName = scan.nextLine();
-
-
+        String user = makeUserName(fullName);
+        System.out.println("Enter your email provider (ie. ssfs.org):  ");
+        String provider = scan.nextLine();
+        String email = makeEmail(user,provider);
+        System.out.println("New email address: " + email);
     }
+    public static String makeUserName(String name){
+        String initial = name.substring(0,1);
+        String initial2 = initial.toLowerCase();
+        int space = name.indexOf(" ");
+        String last = name.substring(space+1);
+        String last2 = last.toLowerCase();
+        int num = (int) (Math.random()*90)+ 10;
+        return initial2 + last2 + num;
+
+        }
+    public static String makeEmail(String user, String provider){
+        return user + "@" + provider;
+        }
+
+
+
+
 }
